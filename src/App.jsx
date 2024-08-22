3b4cd0faf6b1271795434401332e959d50f0f421
vaishnavi04 <vaishnavisakthivel1606@gmail.com>
2024-08-22 22:36:36 +0530

    Login

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import './App.css';
 import Login from './Login';
+
 function App() {
   return (
     <div className="App">
      
      <Login/>
+  
     
     </div>
   );
--- src/Login.jsx
+++ src/Login.jsx
@@ -1,21 +1,57 @@
-import React from 'react'
+import React,{useState}from 'react'
 import './App.css'; 
+// import VisibilityIcon from '@mui/icons-material/Visibility';
+import HttpsIcon from '@mui/icons-material/Https';
+
 
 function Login() {
+  const [username,setUsername]= useState("");
+  const [password,setPassword]=useState("");
+  const handleSubmit =(event)=>{
+    event.preventDefault();
+    console.log(username);
+    console.log(password);
+  }
   return (
     <div>
         <div class="container">
-            <h1>Login page</h1>
+            <h1>Login</h1>
             <div class="form">
-              <label htmlFor="username">Username:  </label>
+              <form className='form' onSubmit={handleSubmit}>
+                <label htmlFor="username">Username:</label>
               <input type="username"
-               placeholder='enter your username'>
-              </input><br/><br/>
-              <label htmlFor="password">Password: </label>
+               placeholder='enter your username'
+               value={username} onChange={(e)=>setUsername(e.target.value)}>
+              </input>
+             
+              <label htmlFor="password">Password:</label>
               <input type="password"
-              placeholder='enter your password'></input>
+               placeholder='enter your password'
+               value={password} onChange={(e)=>setPassword(e.target.value)}>
+              
+              </input>
+             < HttpsIcon className='icon'/>
+
+          
+              
+              {/* <VisibilityIcon/> */}
               <br/><br/>
-              <button type="submit">Submit</button>
+              <div className="forget-pass">
+                <label><input type="checkbox"/>Remember me</label>
+                <br/>
+                <a href="#">Forget Password</a>
+              </div><br/><br/>
+              <div className="Login">
+              <button type="submit">Login</button>
+              </div>
+              <div className="signup">
+                <h4>Don't have an account :</h4>
+              
+                <a href="#">Signup</a>
+                
+                
+                </div>
+              </form>
             </div>
   
         </div>
